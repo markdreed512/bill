0888a6d6d3064622bb06aadcdf843b4ba437d120
Mark Reed <markdreed512@gmail.com>
2019-06-19 17:40:41 -0500

    Updates

---
+++ precache-manifest.2c6939e5843984e5d4bb06c510248109.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a957d686ceb6042b6c16e1d09e17850e",
+    "url": "/click-a-bill/index.html"
+  },
+  {
+    "revision": "b4639706373f504e873b",
+    "url": "/click-a-bill/static/css/main.ec42e490.chunk.css"
+  },
+  {
+    "revision": "9508b3e097f5d4db1edd",
+    "url": "/click-a-bill/static/js/2.97807068.chunk.js"
+  },
+  {
+    "revision": "b4639706373f504e873b",
+    "url": "/click-a-bill/static/js/main.96942e75.chunk.js"
+  },
+  {
+    "revision": "5f3c495818882772e650",
+    "url": "/click-a-bill/static/js/runtime~main.95bd7d95.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/bill/precache-manifest.f7924efed89f8c40184b41804d97c793.js"
+  "/click-a-bill/precache-manifest.2c6939e5843984e5d4bb06c510248109.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bill/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/click-a-bill/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
